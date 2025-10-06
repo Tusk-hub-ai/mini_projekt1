c72ddf61337fdc333b4524871ab1e65b60e3e037
Tusk-hub-ai <you@example.com>
2025-10-06 23:12:39 +0200

    new version

--- clock.py
+++ clock.py
@@ -53,6 +53,13 @@ def endpos(angle,mark):
     end_y_pos = center_point[1] + mark * math.sin(math.radians(angle))
     end_pos = (end_x_pos,end_y_pos)
     return end_pos
+#rectangle function
+def rec(color,start,end,width,lenght):
+    pygame.draw.rect(screen,color,(start,end,width,lenght))
+panel_labels = ["DESTINATION TIME", "PRESENT TIME", "LAST TIME DEPARTED"]
+digifont = pygame.font.Font("digital-7.ttf",60)
+# Default times
+destination_time = "21-10-1985 07:28"
 
 #main while
 while True:
@@ -64,6 +71,7 @@ while True:
     
     
     #the base circle for the clock
+    base_circle = pygame.draw.circle(screen,(90,90,90),center_point,radius_clock+20)
     base_circle = pygame.draw.circle(screen,(122,122,122),center_point,radius_clock)
     base_circle = pygame.draw.circle(screen,(101,67,33),center_point,radius_clock+2,5)
     
@@ -101,6 +109,60 @@ while True:
     hour = datetime.now().hour
     hand(hour,3,9,(50,20,20),80,12,7)
 
+
+
+
+
+
+    #render bttf time panel 
+    center_point1 = (center_point[0]-600,center_point[1])
+    start_point = (center_point1[0]-300,center_point1[1]-200)
+    rec((50,50,50),start_point[0],start_point[1],600,400)
+    #destination time
+    rec((80,80,80),start_point[0]+5,start_point[1]+10,590,120)
+    rec((0,0,0),start_point[0]+150,start_point[1]+100,300,30)
+    # Render
+    text_surface1 = font.render(panel_labels[0], True, (255, 255, 255))
+    text_rect1 = text_surface1.get_rect(center=(start_point[0]+300,start_point[1]+115))
+    # Draw text
+    screen.blit(text_surface1, text_rect1)
+    #clock present time
+    dest_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    text_surface1 = digifont.render(str(dest_time), True, (255, 0, 0))
+    text_rect1 = text_surface1.get_rect(center=(start_point[0]+300,start_point[1]+70))
+    screen.blit(text_surface1, text_rect1)
+    
+
+    #present time
+    rec((80,80,80),start_point[0]+5,start_point[1]+135,590,120)
+    rec((0,0,0),start_point[0]+175,start_point[1]+225,250,30)
+    # Render the Roman numeral
+    text_surface2 = font.render(panel_labels[1], True, (255, 255, 255))
+    text_rect2 = text_surface2.get_rect(center=(start_point[0]+300,start_point[1]+240))
+    # Draw text
+    screen.blit(text_surface2, text_rect2)
+    #clock present time
+    present_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    text_surface2 = digifont.render(str(present_time), True, (0, 255, 0))
+    text_rect2 = text_surface2.get_rect(center=(start_point[0]+300,start_point[1]+195))
+    screen.blit(text_surface2, text_rect2)
+
+    
+    
+    #last time departed
+    rec((80,80,80),start_point[0]+5,start_point[1]+260,590,120)
+    rec((0,0,0),start_point[0]+125,start_point[1]+350,350,30)
+    # Render the Roman numeral
+    text_surface3 = font.render(panel_labels[2], True, (255, 255, 255))
+    text_rect3 = text_surface3.get_rect(center=(start_point[0]+300,start_point[1]+365))
+    # Draw text
+    screen.blit(text_surface3, text_rect3)
+    #clock present time
+    last_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    text_surface3= digifont.render(str(last_time), True, (255, 255, 0))
+    text_rect3 = text_surface3.get_rect(center=(start_point[0]+300,start_point[1]+320))
+    screen.blit(text_surface3, text_rect3)
+    
     
     pygame.display.flip()
     for event in pygame.event.get():
